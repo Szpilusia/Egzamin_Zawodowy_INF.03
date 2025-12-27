0f6ef13b6ffb8d950e1db96fc1e09569d322ca25
Szpileczka <webs@pixelpick.pl>
2025-12-27 16:09:35 +0100

    Update script.js

--- INF.03_2025/INF.03_Z6_01.25/rozwiazanie/script.js
+++ INF.03_2025/INF.03_Z6_01.25/rozwiazanie/script.js
@@ -16,18 +16,19 @@ function liczenie() {
   while (liczba > 0) {
     binarka = (liczba % 2) + binarka;
     liczba = Math.floor(liczba / 2);
-
-    console.log(binarka);
-    console.log(liczba);
   }
 
   let grupowanie = "";
   for (let i = binarka.length; i > 0; i -= 4) {
     let start = Math.max(i - 4, 0);
     let grupa = binarka.substring(start, i);
-    grupowanie = grupa + (grupowanie ? " " + grupowanie : "");
-    console.log(grupowanie);
+    if (grupowanie !== "") {
+      grupowanie = grupa + " " + grupowanie;
+    } else {
+      grupowanie = grupa;
+    }
   }
 
   wynik.innerHTML = grupowanie + " <sub>(2)</sub>";
 }
+
